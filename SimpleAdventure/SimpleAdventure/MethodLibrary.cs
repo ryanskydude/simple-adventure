c30f9529fb60dad900c560649e61043f95601d82
ryanskydude <51873474+ryanskydude@users.noreply.github.com>
2021-04-09 20:48:08 -0700

    ver 0.5alpha in progress
    this is the very first version of the game in progress.

---
+++ SimpleAdventure/SimpleAdventure/MethodLibrary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SimpleAdventure
+{
+    public class MethodLibrary
+    {
+        public static void Init()
+        {
+            double releaseVersion = 0.5;
+            string branchLevel = "alpha";
+            String title = @"
+
+
+ _____ _                 _      
+/  ___(_)               | |     
+\ `--. _ _ __ ___  _ __ | | ___ 
+ `--. \ | '_ ` _ \| '_ \| |/ _ \
+/\__/ / | | | | | | |_) | |  __/
+\____/|_|_| |_| |_| .__/|_|\___|
+                  | |           
+                  |_|           
+  ___      _                 _                  
+ / _ \    | |               | |                 
+/ /_\ \ __| |_   _____ _ __ | |_ _   _ _ __ ___ 
+|  _  |/ _` \ \ / / _ \ '_ \| __| | | | '__/ _ \
+| | | | (_| |\ V /  __/ | | | |_| |_| | | |  __/
+\_| |_/\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            //this is the code for the title big man
+            Console.WriteLine(title);
+            Console.ResetColor();
+            Console.WriteLine("By SamuraiWeebR");
+            Console.WriteLine("This is A Very Basic Game.");
+            Console.WriteLine("Release Ver: " + releaseVersion + branchLevel);
+            Thread.Sleep(2000);
+        }
+
+
+        public static Int16 InputNumber()
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                return Convert.ToInt16(input);
+            }
+            catch (Exception FormatException)
+            {
+                Console.WriteLine("Please insert a Arabic numeral instead of text.");
+                return InputNumber();
+            }
+        }
+
+        public static void playerUI(string playerStatus, double playerHP, int playerGold, double PlayerARMR)
+        {
+            Console.WriteLine("Player status is: " + playerStatus);
+            Console.WriteLine();
+            Console.WriteLine("HP: " + playerHP + " Gold: " + playerGold + " Armor: " + PlayerARMR);
+        }
+
+        public static void playerDeath()
+        {
+            Console.WriteLine("you have died. relaunch the game to make a new character.");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ SimpleAdventure/SimpleAdventure/titleandprologue.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleAdventure
+{
+    class MainClass : MethodLibrary
+    {
+        public static void Main(string[] args)
+        {
+            //Initiates the game with info and title screen
+            Init();
+
+            //some text and variables are placed here.
+            string UserInput;
+            string charName = "";
+            Int16 charAge = 0;
+            string charSex = "milk";
+            string playerStatus = "Healthy";
+            double playerHP = 20;
+            int playerGold = 5;
+            double playerARMR = 10;
+
+            Console.Clear();
+            /*some really long writeline because im lazy.
+              it also serves as the tutorial*/
+            Console.WriteLine("Welcome to Simple Adventure! \n This is my first game that i have been developing in my spare time. \n This is a text based adventure game, meaning you have to type your actions. \n Remember that not all actions are available, \n and try to be simple when entering a answer. example, inspect object. \n Im pretty new to coding. \n Press any key to continue");
+            Console.ReadKey();
+
+            //this is character creation
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.Clear();
+
+            Console.WriteLine("Welcome to Character Creation! we will start with your name.");
+            UserInput = Console.ReadLine();
+            charName = UserInput;
+            Console.WriteLine("Your name is " + charName + "!");
+            Thread.Sleep(800);
+            Console.WriteLine("What is your age?");
+            charAge = InputNumber();
+            Console.WriteLine("Your age is " + charAge + "!");
+            Thread.Sleep(800);
+            Console.WriteLine("What is your gender?");
+            UserInput = Console.ReadLine(); //this switch is gonna make you mad cause its the only switch in character creation
+            switch (UserInput)
+            {
+                case "male":
+                    charSex = "male";
+                    break;
+
+                case "female":
+                    charSex = "female";
+                    break;
+
+                case "milk":
+                    charSex = "milk";
+                    break;
+
+            }
+            Console.WriteLine("Your gender is " + charSex + "!");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.WriteLine("This is you! \n name: " + charName + " \n age: " + charAge + " \n gender: " + charSex + " \n enjoy the game!");
+            Console.ReadKey();
+
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear(); // this is the start of the game. tullemania helped with the BIG BOI words.
+            //if you're a milk, you're just a milk. you can't do anything.
+            if (charSex == "milk")
+            {
+                //this is art
+                Console.WriteLine("Since you are just milk you are unable to do anything that would require a consciousness. " +
+                    "You may think this is quite the disappointing outcome but is it really any worse than the other possibilities. " +
+                    "Sure you could have gone on a grand adventure and all but remember the choice was yours. " +
+                    "This was all indeed your own doing. You have no one to blame but yourself. " +
+                    "So here you are getting what you deserve. " +
+                    "Having your existence be some spilled milk rotting for each day that goes until you are no more. " +
+                    "Remember and grieved by no one. " +
+                    "Since only a milkdrinker would cry over spilled milk. -Tullemania The intellectually challenged");
+                playerDeath();
+            }
+            Console.WriteLine("You regain your consciousness inside of a damp and dark prison cell, unbeknownst to the reason your here.");
+            playerUI(playerStatus, playerHP, playerGold, playerARMR);
+            //here is how i loop a question if they put the wrong thing, instead of ending the game
+            while (true)
+            {
+                Console.WriteLine("What is your course of action?");
+                UserInput = Console.ReadLine();
+                if (UserInput.ToLower() == "inspect prison cell" || UserInput.ToLower() == "investigate prison cell")
+                {
+                    Console.WriteLine("you take a look at your surroundings. Its a cold, dark and damp prison cell. \n There are bars on a small window, letting out minimal moonlight as your only light source. \n The door is sealed by 3 locks. Next to the door lies some old shackles.");
+                    break;
+                }
+                else if (UserInput.ToLower() == "do a 360 no scope") //funny easter egg
+                {
+                    Console.WriteLine("you do a 360, pull a sniper out of your ass, and no scope. \n the bullet reflects and hits you in the head.");
+                    playerDeath();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("response invaild. Try Again.");
+                }
+            }
+            Thread.Sleep(800);
+            Console.WriteLine("you wait around the cell, unsure what to do. \n suddenly, a person is at the door. \n 'find a way.' he utters in a quiet coarse voice. \n the door unlocks.");
+            playerUI(playerStatus, playerHP, playerGold, playerARMR);
+            while (true)
+            {
+                Console.WriteLine("What is your course of action?");
+                UserInput = Console.ReadLine();
+                if (UserInput.ToLower() == "open door" || UserInput.ToLower() == "exit prison cell")
+                {
+                    Console.WriteLine("you open the door to the prison cell quietly, \n taking a peek into the long dark hallways. \n you notice there are no guards, and look around. \n there are prison cells lining the walls.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("response invaild. Try Again.");
+                }
+            }
+        }
+    }
+
+}
